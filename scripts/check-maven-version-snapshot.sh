590f8d580dbd97d8aec14d52f75543eb576ec33d
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-05-17 17:37:00 +0530

    asss

--- scripts/check-maven-version-snapshot.sh
+++ scripts/check-maven-version-snapshot.sh
@@ -3,12 +3,12 @@ echo "=================================================="
 echo "DEBUG - Reading POM file to obtain current version"
 echo "--------------------------------------------------"
 
-POM_SNAPSHOT_VERSION=${env.POM_VERSION}
+POM_SNAPSHOT_VERSION=${POM_VERSION}
 # POM_SNAPSHOT_VERSION=$(mvn help:evaluate -Dexpression='project.version' -q -DforceStdout);
 echo "DEBUG - Pom version: $POM_SNAPSHOT_VERSION"
-echo "DEBUG - Major version: ${env.POM_MAJOR_VERSION}"
-echo "DEBUG - Minor version: ${env.POM_MINOR_VERSION}"
-echo "DEBUG - Patch version: ${env.POM_PATCH_VERSION}"
+echo "DEBUG - Major version: ${POM_MAJOR_VERSION}"
+echo "DEBUG - Minor version: ${POM_MINOR_VERSION}"
+echo "DEBUG - Patch version: ${POM_PATCH_VERSION}"
 
 # POM_MAJOR_VERSION=$(mvn help:evaluate -Dexpression='project.version' -q -DforceStdout |  cut -d. -f1 );
 # POM_MINOR_VERSION=$(mvn help:evaluate -Dexpression='project.version' -q -DforceStdout |  cut -d. -f2 );
