a95f93260a38abbc31fc00422dfe0313d5c21cd4
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-05-19 12:35:47 +0530

    Update create-release-branch.sh

--- scripts/create-release-branch.sh
+++ scripts/create-release-branch.sh
@@ -11,23 +11,25 @@ MAJOR_VERSION=$(echo $POM_VERSION | cut -d. -f1)
 MINOR_VERSION=$(echo $POM_VERSION | cut -d. -f2)
 PATCH_VERSION=$(echo $POM_VERSION | cut -d. -f3 | cut -d- -f1)
 MAVEN_QUALIFIER=$(echo $POM_VERSION | cut -d- -f2)
-VERSION_WITHOUT_BUILD_NUMBER=$(echo $POM_VERSION | sed 's/^\(.*-\)\([0-9]*\)$/\1/')
-VERSION_BUILD_NUMBER=$(echo $POM_VERSION | sed 's/^\(.*-\)\([0-9]*\)$/\2/')
+VERSION_WITHOUT_BUILD_NUMBER=$(echo $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$MAVEN_QUALIFIER-)
+VERSION_BUILD_NUMBER=$(echo $POM_VERSION | cut -d- -f3)
 
 echo "DEBUG - MAJOR_VERSION: $MAJOR_VERSION"
 echo "DEBUG - MINOR_VERSION: $MINOR_VERSION"
 echo "DEBUG - PATCH_VERSION: $PATCH_VERSION"
 echo "DEBUG - MAVEN_QUALIFIER: $MAVEN_QUALIFIER"
-echo "VERSION_WITHOUT_BUILD_NUMBER: ${env.VERSION_WITHOUT_BUILD_NUMBER}"
-echo "VERSION_BUILD_NUMBER: ${env.VERSION_BUILD_NUMBER}"
+echo "VERSION_WITHOUT_BUILD_NUMBER: ${VERSION_WITHOUT_BUILD_NUMBER}"
+echo "VERSION_BUILD_NUMBER: ${VERSION_BUILD_NUMBER}"
 
 echo "------------------------------------------------------------"
 echo "DEBUG - Create Release Branch"
 echo "------------------------------------------------------------"
 
-if [ ${JOB_NAME} -eq 'bugfix-release' ]
+if [[ $JOB_NAME -eq 'bugfix-release' ]]
 then
-    git checkout -b release/$MAJOR_VERSION.$MINOR_VERSION-${env.VERSION_BUILD_NUMBER} bugfix/$MAJOR_VERSION.$MINOR_VERSION;
+    git add pom.xml   
+    git commit -m "Bugfix release RC-${VERSION_BUILD_NUMBER}"
+    git checkout -b release/$MAJOR_VERSION.$MINOR_VERSION-${VERSION_BUILD_NUMBER} release/$MAJOR_VERSION.$MINOR_VERSION;
 else
     git checkout develop;
     git checkout -b release/$MAJOR_VERSION.$MINOR_VERSION develop;
