15c6635c3fe7a073fa97ac89b291350d04639bb8
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-05-19 12:31:57 +0530

    Update bugfix-update-build-number-and-set-maven-version.sh

--- scripts/bugfix-update-build-number-and-set-maven-version.sh
+++ scripts/bugfix-update-build-number-and-set-maven-version.sh
@@ -11,15 +11,15 @@ MAJOR_VERSION=$(echo $POM_VERSION | cut -d. -f1)
 MINOR_VERSION=$(echo $POM_VERSION | cut -d. -f2)
 PATCH_VERSION=$(echo $POM_VERSION | cut -d. -f3 | cut -d- -f1)
 MAVEN_QUALIFIER=$(echo $POM_VERSION | cut -d- -f2)
-VERSION_WITHOUT_BUILD_NUMBER=$(echo $POM_VERSION | sed 's/^\(.*-\)\([0-9]*\)$/\1/')
-VERSION_BUILD_NUMBER=$(echo $POM_VERSION | sed 's/^\(.*-\)\([0-9]*\)$/\2/')
+VERSION_WITHOUT_BUILD_NUMBER=$(echo $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$MAVEN_QUALIFIER-)
+VERSION_BUILD_NUMBER=$(echo $POM_VERSION | cut -d- -f3)
 
 echo "DEBUG - MAJOR_VERSION: $MAJOR_VERSION"
 echo "DEBUG - MINOR_VERSION: $MINOR_VERSION"
 echo "DEBUG - PATCH_VERSION: $PATCH_VERSION"
 echo "DEBUG - MAVEN_QUALIFIER: $MAVEN_QUALIFIER"
-echo "VERSION_WITHOUT_BUILD_NUMBER: ${env.VERSION_WITHOUT_BUILD_NUMBER}"
-echo "VERSION_BUILD_NUMBER: ${env.VERSION_BUILD_NUMBER}"
+echo "VERSION_WITHOUT_BUILD_NUMBER: ${VERSION_WITHOUT_BUILD_NUMBER}"
+echo "VERSION_BUILD_NUMBER: ${VERSION_BUILD_NUMBER}"
 
 echo "------------------------------------------------------------"
 echo "DEBUG - Incrementing Build Number"
@@ -32,7 +32,7 @@ echo "------------------------------------------------------------"
 echo "DEBUG - Setting New Maven Version"
 echo "------------------------------------------------------------"
 
-mvn build-helper:parse-version versions:set -DnewVersion=$UPDATED_BUILD_NUMBER versions:commit
+mvn build-helper:parse-version versions:set -DnewVersion=$VERSION_WITHOUT_BUILD_NUMBER$UPDATED_BUILD_NUMBER versions:commit
 
 echo "============================================================"
 
